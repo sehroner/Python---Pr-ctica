52db962e0f95b765677878b0e4c8339b45425965
sehroner <sehrmadrid@gmail.com>
2026-04-13 09:18:52 +0200

    Python

---
+++ 1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Introduce el segundo número: "))
+
+resultado = num1 + num2
+
+print("El resultado es:", resultado)
---
+++ 2.py
@@ -0,0 +1,7 @@
+num1=float(input("Introduce el primer número: "))
+num2=float(input("Introduce el segundo número: "))
+
+resultado = round(num1 + num2, 2)
+
+
+print("El resultado es:", resultado)
